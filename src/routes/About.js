2ffc3ee948db9bd87d3516ae2aac2f6039ff9a50
oyabun-dev <khalifaababacarbeye@esp.sn>
2024-04-18 15:22:34 +0000

    implementing dark mode

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function About() {
     return (
-        <div>About</div>
+        <main className='about-page'>
+            <div>About</div>
+            <NavLink className="nav-link" to="/about">About</NavLink>
+            <NavLink className="nav-link" to="
+                /baba">yoo</NavLink>
+            <NavLink className="nav-link" to="/">active</NavLink>
+        </main>
     )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+
 export default function Home() {
+    let theme = document.body.classList.contains('darkMode') ? 'Light Mode' : 'Dark Mode';
+    const [mode, setMode] = useState(theme);
+    const toggleMode = () => {
+        document.body.classList.toggle('darkMode');
+        document.body.classList.contains('darkMode') ? setMode('Light Mode') : setMode('Dark Mode')
+    }
     return (
-        <>
-            <div>Home</div>
-            <NavLink to="/about">About</NavLink>
-        </>
+        <main className='landing'>
+            <div className='heading'>Limited HomePage</div>
+            <div className='subheading'>Limited HomePage</div>
+            <p className="paragraph">This is a paragraph</p>
+            <p className="card-price">$ 125.00</p>
+            <p className="card-discount">$ 250.00</p>
+            <p className="card-discount-rate">50%</p>
+            <p className="item-counter">3</p>
+            <p className='cart-title'>Cart</p>
+            <p className='cart-paragraph'>lorem ipsum dolor <span className='cart-price'>$ 125.00</span></p>
+            <button className='btn-orange'>Click here</button>
+            <NavLink className="nav-link" to="/about">About</NavLink>
+            <NavLink className="nav-link" to="/baba">yoo</NavLink>
+            <NavLink className="nav-link" to="/">active</NavLink>
+            <button className='btn-orange' onClick={toggleMode}>{mode}</button>
+        </main>
     )
 }
